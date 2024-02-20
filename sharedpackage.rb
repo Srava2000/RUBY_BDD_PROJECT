8f493df4118bd154e632bec3f0164021f61a4a8d
Srava2000 <126134820+Srava2000@users.noreply.github.com>
2024-02-20 14:37:37 +0530

    Add files via upload

---
+++ sharedpackage.rb
@@ -0,0 +1,8 @@
+
+
+module SharedPackage
+    def self.feature_files_directory
+        File.expand_path("C://Users//SREDDY31//Desktop//ruby_bdd_project//shared_package//feature_files//feature1.feature", __FILE__)
+    end
+end
+   

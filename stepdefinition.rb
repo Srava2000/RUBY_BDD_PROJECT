28451c7bbad10be5c221178522ecd0d2610f2cd8
Srava2000 <126134820+Srava2000@users.noreply.github.com>
2024-02-20 14:37:15 +0530

    Add files via upload

---
+++ stepdefinition.rb
@@ -0,0 +1,41 @@
+
+Given('chrome browser  is opened and entered url') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click on advanced link') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click on preview safe mode link') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I enter Loginmail') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I enter password') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click on Login button') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('you are in Homepage') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click on product link') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('You are in Product page') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('user is on product page') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
